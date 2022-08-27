f6e7da5f0516d35b170aac574b3eac8bc43dd85d
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-08-27 00:34:46 -0300

    depositShouldDoNothingWhenNegativeAmount

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import entities.Account;
 
 public class AccountTests {
-
+	
 	@Test
 	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
 		
@@ -20,6 +20,18 @@ public class AccountTests {
 		
 		//Assert: declarar o que deve acontecer
 		//biblioteca de declarações de JUnit
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, expectedValue);
+		double amount = -200.0;
+		
+		
+		acc.deposit(amount);
+		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
 	
